346a2d1f32ca794bc8dc36bed3d4926acf337c39
slonermike <michaelgareth@gmail.com>
2024-10-08 13:40:38 -0400

    Isolate transform logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { mat2d, type vec2 } from "gl-matrix"
+import { type vec2 } from "gl-matrix"
 import { Visualizer } from "./components/Visualizer"
+import { Transform, transformToMatrix } from "./transform";
 
 const SQUARE: vec2[] = [[100, 100], [-100, 100], [-100, -100], [100, -100]]
 const App = () => {
-  const translate = mat2d.create()
-  mat2d.fromTranslation(translate, [Math.random() * 250, Math.random() * 500])
+  const translate: Transform = {
+    type: 'move',
+    move: [Math.random() * 250, Math.random() * 500]
+  }
+  const rotate: Transform = {
+    type: 'rotate',
+    rotation: Math.random() * Math.PI * 2
+  }
+  const scale: Transform = {
+    type: 'scale',
+    scale: [1 + Math.random(), 1 + Math.random()]
+  }
 
-  const rotate = mat2d.create()
-  mat2d.fromRotation(rotate, Math.random() * Math.PI * 2)
+  const transforms = [translate, rotate, scale]
 
-  const scale = mat2d.create()
-  mat2d.fromScaling(scale, [1 + Math.random(), 1 + Math.random()])
-
-  return <Visualizer shape={SQUARE} transforms={[translate, rotate, scale]} />
+  return (
+    <div>
+      <Visualizer shape={SQUARE} transforms={transforms.map(x => transformToMatrix(x))} />
+    </div>
+  )
 };
 
 export default App;
---
+++ src/transform.ts
@@ -0,0 +1,38 @@
+
+
+import { type vec2, mat2d } from "gl-matrix";
+
+interface RotateTransform {
+  type: 'rotate',
+  rotation: number
+}
+
+interface ScaleTransform {
+  type: 'scale',
+  scale: vec2
+}
+
+interface MoveTransform {
+  type: 'move',
+  move: vec2
+}
+
+export type Transform = RotateTransform | ScaleTransform | MoveTransform
+
+export function transformToMatrix(transform: Transform): mat2d {
+  const m = mat2d.create()
+  switch(transform.type) {
+    case 'move':
+      mat2d.fromTranslation(m, transform.move)
+      break
+    case 'rotate':
+      mat2d.fromRotation(m, transform.rotation)
+      break
+    case 'scale':
+      mat2d.fromScaling(m, transform.scale)
+      break
+    default:
+      break
+  }
+  return  m
+}
